9601e5b76448c9fc174c2ff6008ef3af7e37aec5
Nguyen <96384770+nguyne@users.noreply.github.com>
2023-03-30 11:06:07 +0700

    data

--- src/main/java/com/busstation/entities/Leave.java
+++ src/main/java/com/busstation/entities/Leave.java
@@ -1,4 +1,47 @@
 package com.busstation.entities;
 
-public class Leave {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_leave")
+@Data
+public class Leave implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "leave_id",length = 36)
+    private String leaveId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "date_start", nullable = false)
+    private Date dateStart;
+    @Column(name = "date_end", nullable = false)
+    private Date dateEnd;
+
+    @Column(name = "reason", nullable = false, length = 100)
+    private String reason;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", columnDefinition = "DATE DEFAULT CURRENT_DATE", nullable = false)
+    private Date createAt;
+    protected void onCreate() { createAt = new Date(); }
+
+    @JsonIgnore
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/busstation/entities/Trip.java
+++ src/main/java/com/busstation/entities/Trip.java
@@ -1,4 +1,38 @@
 package com.busstation.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_trip")
+@Data
 public class Trip {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "trip_id",length = 36)
+    private String tripId;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id", referencedColumnName = "car_id")
+    private Car car;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+    @Column(name = "province_start", nullable = false, length = 20)
+    private String provinceStart;
+    @Column(name = "province_end", nullable = false, length = 20)
+    private String provinceEnd;
+    @Column(name = "time_start", nullable = false)
+    private Date timeStart;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", columnDefinition = "DATE DEFAULT CURRENT_DATE", nullable = false)
+    private Date createAt;
+    protected void onCreate() { createAt = new Date(); }
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "update_at")
+    private Date updateAt;
 }
