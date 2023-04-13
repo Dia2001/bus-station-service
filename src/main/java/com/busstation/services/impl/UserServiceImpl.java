40e25928a8659fb2ed3b7ee0e81b19c4b859e944
Trung <ngtrung502@gmail.com>
2023-04-13 10:29:34 +0700

    function : delete user ( set status user )

--- src/main/java/com/busstation/services/impl/UserServiceImpl.java
+++ src/main/java/com/busstation/services/impl/UserServiceImpl.java
@@ -1,13 +1,11 @@
 package com.busstation.services.impl;
 
-import com.busstation.converter.UserConverter;
 import com.busstation.dto.UserDto;
 import com.busstation.entities.User;
 import com.busstation.enums.NameRoleEnum;
 import com.busstation.exception.DataNotFoundException;
 import com.busstation.payload.request.UserRequest;
 import com.busstation.payload.response.ApiResponse;
-import com.busstation.repositories.AccountRepository;
 import com.busstation.repositories.UserRepository;
 import com.busstation.repositories.custom.UserRepositoryCustom;
 import com.busstation.services.UserService;
@@ -24,35 +22,39 @@ import java.util.Date;
 
 @Service
 public class UserServiceImpl implements UserService {
-    @Autowired
-    private AccountRepository accountRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private UserRepositoryCustom userRepositoryCustom;
-    @Autowired
-    private UserConverter userConverter;
+	@Autowired
+	private UserRepositoryCustom userRepositoryCustom;
 
-    @Override
-    public Page<UserDto> getAlL(String keyword,int pageNumber, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNumber,pageSize, Sort.by("fullName").ascending());
-        Page<User> users =userRepositoryCustom.getFilter(keyword, NameRoleEnum.ROLE_USER.toString(), pageable);
-        Page<UserDto> userDtoList = users.map(UserDto::new);
-        return userDtoList;
-    }
+	@Override
+	public Page<UserDto> getAlL(String keyword, int pageNumber, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by("fullName").ascending());
+		Page<User> users = userRepositoryCustom.getFilter(keyword, NameRoleEnum.ROLE_USER.toString(), pageable);
+		Page<UserDto> userDtoList = users.map(UserDto::new);
+		return userDtoList;
+	}
 
-    @Override
-    @Transactional
-    public ApiResponse edit(String id, UserRequest userRequest) {
-        User user=userRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Can't find this user"));
-        user.setFullName(userRequest.getFullName());
-        user.setPhoneNumber(userRequest.getPhoneNumber());
-        user.setEmail(userRequest.getEmail());
-        user.setAddress(userRequest.getAddress());
-        user.setUpdatedAt(new Date());
-        userRepository.save(user);
-        return new ApiResponse("Updated successfully", HttpStatus.OK);
-    }
+	@Override
+	@Transactional
+	public ApiResponse edit(String id, UserRequest userRequest) {
+		User user = userRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Can't find this user"));
+		user.setFullName(userRequest.getFullName());
+		user.setPhoneNumber(userRequest.getPhoneNumber());
+		user.setEmail(userRequest.getEmail());
+		user.setAddress(userRequest.getAddress());
+		user.setUpdatedAt(new Date());
+		userRepository.save(user);
+		return new ApiResponse("Updated successfully", HttpStatus.OK);
+	}
+
+	@Override
+	public ApiResponse setStatus(String id) {
+		User user = userRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Can't find this user"));
+		user.setStatus(false);
+		userRepository.save(user);
+		return new ApiResponse("Set status user successfully", HttpStatus.OK);
+	}
 }
