86bb24fe9bc2d01733d242a9e8ced52e9167e3b6
vo-ngoc-khanh <khanhvo02909@gmai.com>
2023-04-17 21:11:32 +0700

    feat: export-import csv file from data users

--- src/main/java/com/busstation/controller/UserController.java
+++ src/main/java/com/busstation/controller/UserController.java
@@ -1,13 +1,26 @@
 package com.busstation.controller;
 
+import com.busstation.exception.DataExistException;
 import com.busstation.payload.request.UserRequest;
 import com.busstation.payload.response.ApiResponse;
+import com.busstation.services.CsvFileService;
 import com.busstation.services.UserService;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin(origins = "http://localhost:9999")
 @RestController(value = "userAPIofWeb")
 @RequestMapping("/api/v1/users")
@@ -15,6 +28,9 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private CsvFileService csvFileService;
 
 	@GetMapping()
 	@PreAuthorize("hasAnyRole('ROLE_EMPLOYEE','ROLE_ADMIN')")
@@ -40,4 +56,32 @@ public class UserController {
 	public ResponseEntity<ApiResponse> setStatusUser(@PathVariable("userId") String userId) {
 		return new ResponseEntity<>(userService.setStatus(userId), HttpStatus.OK);
 	}
+	
+	@GetMapping("/export-to-csv")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	public void exportUsersCSV(HttpServletResponse response) throws IOException {
+		response.setContentType("text/csv");
+		response.addHeader("Content-Disposition", "attachment; filename=\"users.csv\"");
+	    Writer writer = new OutputStreamWriter(response.getOutputStream(), StandardCharsets.UTF_8);
+	    csvFileService.exportUsesToCsv(writer);
+	}
+
+	@PostMapping("/import-csv-file")
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String importCsvFile(@RequestParam("file") MultipartFile file) {
+        try {
+            // Create a BufferedReader for the uploaded file
+            BufferedReader reader = null;
+			try {
+				reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+            // Call the importCsvFile method of CsvImportService to import data
+            csvFileService.importUserstoCsvFile(reader);
+            return "Import Successfull";
+        } catch (DataExistException ex) {
+            return "Error "+ ex.getMessage();
+        }
+    }
 }
